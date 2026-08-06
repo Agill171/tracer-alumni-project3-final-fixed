a3632c9ab485b3b407bdfdd1bc55e21706ed5baa
Agill171 <80940757+Agill171@users.noreply.github.com>
2026-08-07 03:05:31 +0700

    Menyiapkan akun admin production

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,14 +4,22 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        User::factory()->create([
-            'name' => 'Administrator Tracer Alumni',
-            'email' => 'admin@traceralumni.test',
-        ]);
+        $email = env('ADMIN_EMAIL', 'admin@traceralumni.test');
+        $password = env('ADMIN_PASSWORD', 'password');
+
+        User::updateOrCreate(
+            ['email' => $email],
+            [
+                'name' => 'Administrator Tracer Alumni',
+                'email_verified_at' => now(),
+                'password' => Hash::make($password),
+            ]
+        );
     }
 }
